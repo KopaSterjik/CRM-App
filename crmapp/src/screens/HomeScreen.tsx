6f75e3662a228cef1338b9572309c1d379e3237e
Kopa Sterjik <linegod8@gmail.com>
2025-10-02 23:32:26 +0300

    add sort func

--- crmapp/src/screens/HomeScreen.tsx
+++ crmapp/src/screens/HomeScreen.tsx
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
 import { Text, View, Button, FlatList } from "react-native";
+import { Type } from "../types/Type";
 export default function HomeScreen({ navigation }) {
-  const [item, setItem] = useState("");
+  const [tasks, setTasks] = useState<Type[]>([]);
+  const [sortBy, setSortBy] = useState<"createdAt" | "status">("createdAt");
 
+  const sortTasks = (tasklist: Type[] sortBy: typeof sortBy) => {
+    const sorted = [...tasklist]
+    if(sortBy === "createAt"){
+      sorted.sort((a,b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime())
+    }
+    else if (sortBy === "status"){
+      const statusOrder = ["canceled", "in_progress", "completed"];
+      sorted.sort((a,b) => statusOrder.indexOf(a.status) - statusOrder.indexOf(b.status))
+    }
+    return sorted;
+  }
   return (
     <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
